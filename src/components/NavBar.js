ff2e38e8343e88b78e87151020c247f54a944c16
Exterminatio <shaurma90@mail.ru>
2023-04-14 03:05:08 +0400

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ export const NavBar = () => {
     <nav className="lg:pl-20 lg:pt-10 pb-10 pl-10 pt-5">
       <ul>
         <li className="inline-block lg:text-lg text-sm font-semibold mr-14 hover:text-natural-yellow">
-          <Link href="/">🏠 Home</Link>
+          <Link href="./">🏠 Home</Link>
         </li>
         <li className="inline-block lg:text-lg text-sm font-semibold mr-14 hover:text-natural-yellow">
           <Link href="resume">📋 Resume</Link>
