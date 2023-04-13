635d791687c33449d82818530ea83e01560529f6
Exterminatio <shaurma90@mail.ru>
2023-04-14 02:10:14 +0400

    123

--- next.config.mjs
+++ next.config.mjs
@@ -6,4 +6,11 @@ const nextConfig = {
 module.exports = nextConfig
 module.exports = {
   distDir: 'build',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
 }
+
+export default nextConfig;
